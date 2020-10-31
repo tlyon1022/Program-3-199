209ddd4b681aa3c9dd2d9949ed4d5078775f0e6d
Tanner Lyon <54088092+tlyon1022@users.noreply.github.com>
2020-10-31 09:14:36 -0400

    Update RegForm.cs

--- Prog3 (199)/Prog3/RegForm.cs
+++ Prog3 (199)/Prog3/RegForm.cs
@@ -8,18 +8,7 @@
 // and last name.
 // Decisions based on UofL Spring 2020 Priority Registration Schedule
 
-// Solution 3
-// This solution keeps the first letter of the last name as a char
-// and uses if/else logic for the times.
-// It uses defined strings for the dates and times to make it easier
-// to maintain.
-// It only uses programming elements introduced in the text or
-// in class.
-// This solution takes advantage of the fact that there really are
-// only two different time patterns used. One for juniors and seniors
-// and one for sophomores and freshmen. The pattern for sophomores
-// and freshmen is complicated by the fact the certain letter ranges
-// get one date and other letter ranges get another date.
+
 
 using System;
 using System.Collections.Generic;
